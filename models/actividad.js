1443b9ed6865fc1bad4a637e22f698154247ef89
Cesar Mercado <cesarmercadook@gmail.com>
2024-06-23 21:17:15 -0300

    Añadido atributo del model actividad
    Se agregó el atributo valorCuotaActividad al model actividad

--- models/actividad.js
+++ models/actividad.js
@@ -4,7 +4,8 @@ const {Schema} = mongoose;
 const ActividadSchema = new Schema({
     nombreActividad: {type: String, required: true},
     responsableActividad: {type: String, required: true},
-    horarioActividad: {type: String, required: true}
+    horarioActividad: {type: String, required: true},
+    valorCuotaActividad: {type: Number, required: true}
 })
 
 module.exports = mongoose.models.Actividad || mongoose.model('Actividad', ActividadSchema);
